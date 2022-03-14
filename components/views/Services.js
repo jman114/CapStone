1813ce32abf81014b42194c3f9e404779f74a94e
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-03-14 14:28:51 -0500

    horrible attempt at an API on the Services page

--- components/views/Services.js
+++ components/views/Services.js
@@ -1,3 +1,22 @@
+fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "quotes15.p.rapidapi.com",
+    "x-rapidapi-key": "3fd25e5b47mshe37d9ddea1b4a88p1bb5fbjsnbe96a527108f"
+  }
+})
+  .then(response => response.json())
+  .then(response => {
+    console.log(response);
+
+    document.getElementById("quote").innerHTML = response.content;
+    document.getElementById("author").innerHTML =
+      "- " + response.originator.name + " -";
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
 import html from "html-literal";
 
 export default st =>
@@ -6,21 +25,26 @@ export default st =>
       <h2>Click Below To Book An Appointment</h2>
       <a href="./Book">Book Now</a>
     </section>
-    <table id="services">
+    <h2>This Is A Random Quote Generator Using An API Call - Just Refresh</h2>
+    <div class="quoteContainer">
+      <div id="quote"></div>
+      <div id="author"></div>
+    </div>
+    <!-- <table id="services">
       <tr>
         <th>Men's Hair Cut</th>
         <th>Women's Hair Cut</th>
         <th>Men's Shave</th>
         <th>Coloring</th>
-      </tr>
-      <!-- Using as an example for when I implement my API ${st.services
-        .map(services => {
-          return `<tr><td>${services.crust}</td><td>${
-            services.cheese
-          }</td><td>${services.sauce}</td><td>${services.toppings.join(
-            " & "
-          )}</td><td>${services.customer}</td></tr>`;
-        })
-        .join("")} -->
-    </table>
+      </tr> -->
+    <!-- Using as an example for when I implement my API ${st.services
+      .map(services => {
+        return `<tr><td>${services.crust}</td><td>${services.cheese}</td><td>${
+          services.sauce
+        }</td><td>${services.toppings.join(" & ")}</td><td>${
+          services.customer
+        }</td></tr>`;
+      })
+      .join("")} -->
+    <!-- </table> -->
   `;
--- index.js
+++ index.js
@@ -74,6 +74,7 @@ function addEventListeners() {
 //     }
 //   }
 // });
+
 router
   .on({
     "/": () => render(state.Home),
