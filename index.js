f0df1c5393cb1d5c249083bb6093ba21764c85dd
James McMinn <jmcminn@matchboxdesigngroup.com>
2022-03-18 13:17:57 -0500

    commented out dotenv.config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { capitalize } from "lodash";
 import axios from "axios";
 
 import dotenv from "dotenv";
-dotenv.config();
+//dotenv.config();
 
 const router = new Navigo("/");
 function render(st = state.Home) {
